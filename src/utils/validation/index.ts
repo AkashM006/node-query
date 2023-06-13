f3de9cd7da5caf75e21a400172936b324a0e69b2
Akash M <akashm006@outlook.com>
2023-06-13 16:05:21 +0530

    Minor bugs when migrating to yup from zod

--- src/utils/validation/index.ts
+++ src/utils/validation/index.ts
@@ -1,48 +1,4 @@
-import { z } from "zod";
 import * as yup from "yup";
-
-// const userRegistrationSchema = z.object({
-//   name: z
-//     .string({
-//       required_error: "Name is required",
-//     })
-//     .min(2, "Name should be atleast 3 characters long")
-//     .trim()
-//     .regex(/^[a-zA-z ]{3,}$/, {
-//       message:
-//         "Name should contain only alphabets and length should be atleast 2 characters long",
-//     }),
-//   email: z
-//     .string({
-//       required_error: "Email is required",
-//     })
-//     .trim()
-//     .email("Please enter valid email"),
-//   password: z
-//     .string({
-//       required_error: "Password is required",
-//     })
-//     .trim()
-//     .min(8, "Password must be atleast 8 characters long")
-//     .regex(
-//       /^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[A-Z]).*$/,
-//       "Password must have at least one number, one special character, and one capital letter"
-//     ),
-// });
-
-// const noteSchema = z.object({
-//   title: z
-//     .string({
-//       required_error: "Title of the note is required",
-//     })
-//     .min(4, "Title of the note should be atleast 4 characters long")
-//     .trim(),
-//   body: z
-//     .string({
-//       required_error: "Body of the note is required",
-//     })
-//     .min(5, "Body of the note should be atleast 5 characters long"),
-// });
 
 const noteSchema = yup.object({
   title: yup
@@ -58,5 +14,3 @@ const noteSchema = yup.object({
 });
 
 export { noteSchema };
-
-// export { userRegistrationSchema };
